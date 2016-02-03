a742c116387a2fe0e5b1a626ccf7eb66a5953c81
Gal Schlezinger <schniz@gmail.com>
2016-02-03 20:50:24 +0200

    :book:

--- MSA/src/Utilities/Users.h
+++ MSA/src/Utilities/Users.h
@@ -1,3 +1,12 @@
+/*
+ * Users.h
+ *
+ * Wraps the "users" and "passwords" files in a simple class form
+ *
+ *  Created on: Jan 30, 2016
+ *      Author: Tom Boldan & Gal Schlezinger
+ */
+
 #ifndef USERS_H_
 #define USERS_H_
 
@@ -12,13 +21,40 @@ using namespace std;
 class Users {
 private:
 	static map<string, string>* userToPassword;
+
+	/**
+	 * Caches the file system calls
+	 */
 	static map<string, string>* getAll();
 public:
+	/**
+	 * Loads the text files to the map
+	 */
 	static bool load();
+
+	/**
+	 * Check if user exists
+	 */
 	static bool contains(string name);
+
+	/**
+	 * Create a new user
+	 */
 	static bool create(string name, string password);
+
+	/**
+	 * Check if there is a match with a username and password
+	 */
 	static bool login(string name, string password);
+
+	/**
+	 * Get all the users registered
+	 */
 	static vector<string> getAllNames();
+
+	/**
+	 * Get all the users registered in a textual representation
+	 */
 	static string getAllUsers();
 };
 
